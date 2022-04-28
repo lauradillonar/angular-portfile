c22f86b99601cc06e17cc3c512c8e403eeb8d50d
lauradillonar <laura.dillon.ar@gmail.com>
2022-04-28 12:42:51 -0300

    Creo un componente para cada section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HelloComponent } from './components/hello/hello.component';
+import { AboutmeComponent } from './components/aboutme/aboutme.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { EducationComponent } from './components/education/education.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HelloComponent,
+    AboutmeComponent,
+    ProjectsComponent,
+    ExperienceComponent,
+    EducationComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
