45559d26e21e6c1e857a9e906df1160b1728ecc6
lauradillonar <laura.dillon.ar@gmail.com>
2022-04-29 22:47:13 -0300

    Resuelvo animación de skills en about

--- src/app/components/aboutme/aboutme.component.ts
+++ src/app/components/aboutme/aboutme.component.ts
@@ -12,7 +12,7 @@ export class AboutmeComponent implements OnInit {
   
   myPortfile:any;
   skills:any = [];
-  
+  values: number[] = [10,20,30,40,50,60,70,80,90,100];
 
   ngOnInit(): void {
     this.data.getData().subscribe(data =>{
