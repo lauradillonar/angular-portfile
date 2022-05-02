63befd01967a5576c2346816fcc92b7291a4e130
lauradillonar <laura.dillon.ar@gmail.com>
2022-05-02 11:50:16 -0300

    deploy

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { Firestore} from '@angular/fire/firestore';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor() { }
+}
